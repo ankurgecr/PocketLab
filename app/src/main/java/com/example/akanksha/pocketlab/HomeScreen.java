ecce2128ff53cf4797fc5acfff70cb6a19f49b94
Melissa Regalia <melissa.d.regalia@gmail.com>
2016-04-20 15:26:14 -0700

    Screen that asks for new experiment name & sql class

--- app/src/main/java/com/example/akanksha/pocketlab/HomeScreen.java
+++ app/src/main/java/com/example/akanksha/pocketlab/HomeScreen.java
@@ -1,6 +1,8 @@
 package com.example.akanksha.pocketlab;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,11 +10,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.concurrent.ExecutionException;
 
 
 public class HomeScreen extends ActionBarActivity {
     Button newExpButton;
-    Activity homeScreenSelf = this;
+    Activity mSelf = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,85 @@ public class HomeScreen extends ActionBarActivity {
 
         newExpButton = (Button) findViewById(R.id.new_experiment_button);
 
-        newExpButton.setOnClickListener(new View.OnClickListener() {
+        /*newExpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(homeScreenSelf, NewExperiment.class);
                 startActivity(intent);
+            }
+        });*/
+        newExpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // from http://www.androidsnippets.com/prompt-user-input-with-an-alertdialog
+                final AlertDialog.Builder alert = new AlertDialog.Builder(mSelf);
+
+                //alert.setTitle("Title");
+                alert.setTitle("Enter a name for your new experiment");
+                alert.setMessage("");
+
+                // Set an EditText view to get user input
+                final EditText input = new EditText(mSelf);
+                alert.setView(input);
+
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton)
+                    {
+
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                // from http://stackoverflow.com/questions/2620444/how-to-prevent-a-dialog-from-closing-when-a-button-is-clicked
+                final AlertDialog dialog = alert.create();
+                dialog.show();
+
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        String exp = input.getText().toString();
+                        boolean success = false;
+
+                        if(exp.length() >= 40) {
+                            dialog.setMessage("Too long, must not exceed 40 characters");
+                        }
+                        if (exp.equals("")) {
+                            dialog.setMessage("Experiment must have a name");
+                        }
+                        else{
+                            NewExperimentSQL s = new NewExperimentSQL();
+                            s.execute(MainActivity.currentUser, exp);
+                            String result = "Works";//s.get();
+
+                            if (result.equals("Works")) {
+                                dialog.dismiss();
+                                AlertDialog.Builder dialog2 = new AlertDialog.Builder(mSelf);
+                                //alert.setTitle("Title");
+                                dialog2.setTitle("Made new experiment \"" + exp + "\"!");
+
+                                dialog2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        Intent intent = new Intent(mSelf, NewExperiment.class);
+                                        startActivity(intent);
+                                    }
+                                });
+
+                                dialog2.show();
+                            }
+                        }
+                    }
+                });
             }
         });
     }
---
+++ app/src/main/java/com/example/akanksha/pocketlab/NewExperimentSQL.java
@@ -0,0 +1,77 @@
+package com.example.akanksha.pocketlab;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by melissaregalia on 4/20/16.
+ */
+public class NewExperimentSQL extends AsyncTask<String, Void, String> {
+    private static final String POSTGRESS_DRIVER = "org.postgresql.Driver";
+
+    TextView resultArea;
+    String mQuery;
+    String loginname;
+
+    //here is the intialization arguments - pass in a textview to initialize it
+    //make this an error slot
+    public NewExperimentSQL() {
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String retval = "";
+        try {
+            Class.forName(POSTGRESS_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            retval = e.toString();
+        }
+        String url = "jdbc:postgresql://ec2-174-129-26-115.compute-1.amazonaws.com:5432/d4hp0ep351mjmr?user=mlcqisdxxxgoct&password=D1Cu5DZU0oi9Vy1L5QjY3WsbHU&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+        Connection conn;
+        try {
+            DriverManager.setLoginTimeout(5);
+            conn = DriverManager.getConnection(url);
+            Statement st = conn.createStatement();
+            String user, expname,recievedstring = "", sql, sqlcheck;
+            user = params[0]; //string for inserting into table
+            expname = params[1];
+            long time = System.currentTimeMillis();
+
+            sql = "INSERT INTO expdata VALUES = (\'"+user+"\',"+time+",\'"+expname+"\',\'\',\'\',\'\',\'\')";
+            sqlcheck = "SELECT * from expdata WHERE user =\'" + user + "\' AND timestamp =" + time;
+
+            //add the new experiment
+            st.executeUpdate(sql);
+            //now check and verify that it was added
+            ResultSet rs = st.executeQuery(sqlcheck);
+            while (rs.next()){
+                recievedstring = (rs.getString("expname")).trim();
+            }
+            //if failed to add
+            if(!recievedstring.equals(expname)){
+                st.close();
+                conn.close();
+                return "failed to add";
+            }
+            //if added correctly
+            retval = "Works";
+            st.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            retval = e.toString();
+        }
+        return retval;
+    }
+    @Override
+    protected void onPostExecute(String value) {
+        //resultArea.setText(value);
+    }
+}
