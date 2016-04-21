09288e4438fcfbb4f487b16c00e0b3107e1716f6
Akanksha Trivedi <atrivedi@ucdavis.edu>
2016-04-20 18:21:04 -0700

    Change to use native camera; currently pictures are saved in the private app folder

--- app/src/main/java/com/example/akanksha/pocketlab/ColorSensor.java
+++ app/src/main/java/com/example/akanksha/pocketlab/ColorSensor.java
@@ -3,20 +3,31 @@ package com.example.akanksha.pocketlab;
 import android.app.Activity;
 import android.content.Intent;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// http://developer.android.com/training/camera/photobasics.html
 
 public class ColorSensor extends ActionBarActivity {
 
     Button captureButton;
     Activity colorSensorSelf = this;
-    private Camera mCamera = null;
+    //private Camera mCamera = null;
     private CameraView mCameraView = null;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +38,30 @@ public class ColorSensor extends ActionBarActivity {
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(colorSensorSelf, CameraActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(colorSensorSelf, CameraActivity.class);
+                //startActivity(intent);
+
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+
+                    try{
+                        photoFile = createImageFile();
+                    }
+                    catch(IOException e){
+                        Log.d("ERROR", "Error occurred while creating file");
+                    }
+
+                    if(photoFile != null)
+                    {
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                        startActivityForResult(intent,1);
+                    }
+
+                    startActivityForResult(intent, 1);
+                }
+
+
             }
         });
     }
@@ -53,5 +86,21 @@ public class ColorSensor extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "PocketLab_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
