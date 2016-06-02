a0d814e1aed9234a6cfedbbc926d929a205945b7
Melissa Regalia <melissa.d.regalia@gmail.com>
2016-06-02 03:30:14 -0700

    Removed bar

--- app/src/main/java/com/example/akanksha/pocketlab/VariableSensorAct.java
+++ app/src/main/java/com/example/akanksha/pocketlab/VariableSensorAct.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 
-public class VariableSensorAct extends ActionBarActivity {
+public class VariableSensorAct extends Activity {
 
     Button getdata;
     EditText pluspin;
@@ -71,25 +71,4 @@ public class VariableSensorAct extends ActionBarActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_variable_sensor, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
